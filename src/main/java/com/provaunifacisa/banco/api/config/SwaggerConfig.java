1f59859fe2a554416992d90b1513efb5ccfc12a2
Pablo Herivelton <pherivelton@gmail.com>
2021-05-23 08:31:31 -0300

    Configurando o swagger

---
+++ src/main/java/com/provaunifacisa/banco/api/config/SwaggerConfig.java
@@ -0,0 +1,46 @@
+package com.provaunifacisa.banco.api.config;
+
+import java.util.ArrayList;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+@EnableSwagger2	
+public class SwaggerConfig {
+
+	@Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.provaunifacisa.banco.api"))
+                .paths(PathSelectors.ant("/v1.*"))
+                .build()
+                .apiInfo(metaInfo());
+    }
+
+	private ApiInfo metaInfo() {
+
+        @SuppressWarnings("rawtypes")
+		ApiInfo apiInfo = new ApiInfo(
+                "API Banco Unifacisa",
+                "API de manipulação de usuário, suas contas e suas transações",
+                "1.0",
+                "Terms of Service",
+                new Contact("Pablo Herivelton", "www.google.com", "pherivelton@gmail.com"),
+                "Apache License Version 2.0",
+                "https://www.apache.org/licesen.html", new ArrayList<VendorExtension>()
+        );
+
+        return apiInfo;
+    }
+}
